822c7d690291716f8ffb7af68b2299ac8f37aed5
Brian Mantay <brian.mantay@readify.net>
2017-07-10 07:23:56 +1000

    update dot net version to be installed on host

--- infrastructure/setup.ps1
+++ infrastructure/setup.ps1
@@ -11,7 +11,7 @@ Install-WindowsFeature web-server -IncludeManagementTools
 # Install dot.net core sdk
 Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
 Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
-Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848827 -outfile c:\temp\dotnet-dev-win-x64.1.0.4.exe
-Start-Process c:\temp\dotnet-dev-win-x64.1.0.4.exe -ArgumentList '/quiet' -Wait
-Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
-Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://download.microsoft.com/download/D/0/2/D028801E-0802-43C8-9F9F-C7DB0A39B344/dotnet-win-x64.1.1.2.exe -outfile c:\temp\dotnet-win-x64.1.1.2.exe
+Start-Process c:\temp\dotnet-win-x64.1.1.2.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848766 -outfile c:\temp\DotNetCore.1.0.5_1.1.2-WindowsHosting.exe
+Start-Process c:\temp\DotNetCore.1.0.5_1.1.2-WindowsHosting.exe -ArgumentList '/quiet' -Wait
